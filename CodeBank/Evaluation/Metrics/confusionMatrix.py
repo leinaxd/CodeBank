d3455bf7fd97bb6c8690d45dfbe9caf7d0404363
leinaxe <leinaxd@gmail.com>
2022-11-19 03:51:14 -0300

    modified:   CodeBank/Evaluation/Metrics/confusionMatrix.py

--- CodeBank/Evaluation/Metrics/confusionMatrix.py
+++ CodeBank/Evaluation/Metrics/confusionMatrix.py
@@ -43,6 +43,8 @@ class confusionMatrix:
     def __call__(self, 
         predicted:Union[list,int], 
         true_label:Union[list,int]):
+        #TODO:
+        # if predicted/true_label are a batch of tensors? they do not cast to a list...
         if not isinstance(predicted, list):  predicted = [predicted]
         if not isinstance(true_label, list): true_label = [true_label]
         assert len(predicted) == len(true_label), f"predicted and true_label missmatched in size"
