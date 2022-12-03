2a716c94e8e5bbb25104d8dded0b851354391042
leinaxe <leinaxd@gmail.com>
2022-12-03 04:32:38 -0300

    modified:   CodeBank/DatasetFramework/DataAugmentation/txtAugmentation.py

--- CodeBank/DatasetFramework/DataAugmentation/txtAugmentation.py
+++ CodeBank/DatasetFramework/DataAugmentation/txtAugmentation.py
@@ -1,7 +1,11 @@
-import pandas as pd
 
+import torch
+import pandas as pd
+from transformers import AutoModelForMaskedLM, AutoTokenizer
+from transformers import logging
 from CodeBank.DatasetFramework.DataAugmentation.transformations import txtRandomSampling
- 
+from CodeBank.Math.Random import choices
+
 
 class txtAugmentation:
     """
@@ -10,26 +14,63 @@ class txtAugmentation:
         -synonyms
 
     <HuggingFaceLM>: Callable [string with [mask]-> string]
+
+   Possible BUG:
+      que las probabilidades de la transformacion esten entre 0 y 1
     """
-    def __init__(self, transformations:dict, HuggingFaceLM:str, maskToken:str):
+    def __init__(self, path:str, transformation:dict, verbose=False):
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  
+        logging.set_verbosity_error() #Ignore unused weights warning. (this model is for finetunning)
+        self.model = AutoModelForMaskedLM.from_pretrained(path)
+        self.model.to(self.device)
+        # self.model.config.max_position_embeddings = 512
+        logging.set_verbosity_warning()
+        self.tokenizer = AutoTokenizer.from_pretrained(path)
+        self.sampling = txtRandomSampling(transformation['synonyms'], maskToken=self.tokenizer.mask_token)
+        self.selection = choices('softmax')
+        self.verbose=verbose
+        self.count  = 0
+        self.max_length=512
+    # def __init__(self, transformations:dict, HuggingFaceLM:str, maskToken:str):
 
-        self.sampling = txtRandomSampling(transformations['synonyms'], maskToken)
-        self.modelLM = HuggingFaceLM
-        
-    def __call__(self, data: pd.Series):
-        return self.sampling(data)
-        # return data
+    #     self.sampling = txtRandomSampling(transformations['synonyms'], maskToken)
+    #     self.modelLM = HuggingFaceLM
 
+    def doSynonyms(self, txt:str):
+        if self.verbose: 
+            self.count+=1
+            print(f"{self.count}",end='')
+        input_txt = self.sampling(txt)
+        input = self.tokenizer(input_txt, return_tensors = 'pt', truncation=True,max_length=self.max_length)
+        input = {k:v.to(self.device) for k, v in input.items()}
+
+        input_ids = input['input_ids'][0] #ignore batching dim
+        masked_ix = torch.where(input_ids == self.tokenizer.mask_token_id)
+
+        self.model.eval()
+        predictions = self.model(**input).logits
+
+        for ix in masked_ix:
+            predicted_token = self.selection(predictions[0,ix])
+            input_ids[ix] = predicted_token
+
+        tokens = self.tokenizer.convert_ids_to_tokens(input_ids[1:-1])
+        txt = self.tokenizer.convert_tokens_to_string(tokens)
+        return txt
+
+    def __call__(self, data:pd.Series):
+        out = []
+        for sample in data:
+            out.append( self.doSynonyms(sample) )
+        return pd.Series(out)
 
 if __name__ =='__main__':
-    test = 1
-    data = pd.DataFrame({
-            'corpus':['This is an example text', 'Esto es otro texto de ejemplo']
-        })
-    if test == 1:
-        print(f"test {test}: ")
-        transformations={'synonyms':0.5}
-        augmentation = txtAugmentation(transformations, None,'[MASK]')
+    path='BETO/'
+    data=pd.DataFrame({'src':['Hola este es un nuevo día','Unidad anatómica fundamental de todos los organismos vivos, generalmente microscópica, formada por citoplasma, uno o más núcleos y una membrana que la rodea']})
+    transformation={'synonyms':0.1}
+    augmentation = txtAugmentation(path,transformation)
 
-        newData = augmentation(data['corpus'])
-        print(newData)
+    out = augmentation(data['src'])
+    print(out[0])
+    print(out[1])
+
