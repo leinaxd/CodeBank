ba0c14c7dbf64814bc5a58a5d72049f7b092a4a0
leinaxe <leinaxd@gmail.com>
2022-12-05 09:32:09 -0300

    modified:   CodeBank/DatasetFramework/DataAugmentation/txtAugmentation.py

--- CodeBank/DatasetFramework/DataAugmentation/txtAugmentation.py
+++ CodeBank/DatasetFramework/DataAugmentation/txtAugmentation.py
@@ -8,121 +8,38 @@ from CodeBank.DatasetFramework.DataAugmentation.transformations import txtRandom
 from BackTranslation import BackTranslation
 # from CodeBank.Math.Random import choices
 
+import time
 
 class txtAugmentation:
     """
     !pip install BackTranslation
+    sources:
+        https://github.com/hhhwwwuuu/BackTranslation
+        https://neptune.ai/blog/data-augmentation-nlp
     transformations: Dict[type:str, prob:float]
         - synonyms
         - BackTranslation: language
             chinese, english, spanish
-            'af': 'afrikaans',
-            'sq': 'albanian',
-            'am': 'amharic',
-            'ar': 'arabic',
-            'hy': 'armenian',
-            'az': 'azerbaijani',
-            'eu': 'basque',
-            'be': 'belarusian',
-            'bn': 'bengali',
-            'bs': 'bosnian',
-            'bg': 'bulgarian',
-            'ca': 'catalan',
-            'ceb': 'cebuano',
-            'ny': 'chichewa',
-            'zh-cn': 'chinese (simplified)',
-            'zh-tw': 'chinese (traditional)',
-            'co': 'corsican',
-            'hr': 'croatian',
-            'cs': 'czech',
-            'da': 'danish',
-            'nl': 'dutch',
-            'en': 'english',
-            'eo': 'esperanto',
-            'et': 'estonian',
-            'tl': 'filipino',
-            'fi': 'finnish',
-            'fr': 'french',
-            'fy': 'frisian',
-            'gl': 'galician',
-            'ka': 'georgian',
-            'de': 'german',
-            'el': 'greek',
-            'gu': 'gujarati',
-            'ht': 'haitian creole',
-            'ha': 'hausa',
-            'haw': 'hawaiian',
-            'iw': 'hebrew',
-            'he': 'hebrew',
-            'hi': 'hindi',
-            'hmn': 'hmong',
-            'hu': 'hungarian',
-            'is': 'icelandic',
-            'ig': 'igbo',
-            'id': 'indonesian',
-            'ga': 'irish',
-            'it': 'italian',
-            'ja': 'japanese',
-            'jw': 'javanese',
-            'kn': 'kannada',
-            'kk': 'kazakh',
-            'km': 'khmer',
-            'ko': 'korean',
-            'ku': 'kurdish (kurmanji)',
-            'ky': 'kyrgyz',
-            'lo': 'lao',
-            'la': 'latin',
-            'lv': 'latvian',
-            'lt': 'lithuanian',
-            'lb': 'luxembourgish',
-            'mk': 'macedonian',
-            'mg': 'malagasy',
-            'ms': 'malay',
-            'ml': 'malayalam',
-            'mt': 'maltese',
-            'mi': 'maori',
-            'mr': 'marathi',
-            'mn': 'mongolian',
-            'my': 'myanmar (burmese)',
-            'ne': 'nepali',
-            'no': 'norwegian',
-            'or': 'odia',
-            'ps': 'pashto',
-            'fa': 'persian',
-            'pl': 'polish',
-            'pt': 'portuguese',
-            'pa': 'punjabi',
-            'ro': 'romanian',
-            'ru': 'russian',
-            'sm': 'samoan',
-            'gd': 'scots gaelic',
-            'sr': 'serbian',
-            'st': 'sesotho',
-            'sn': 'shona',
-            'sd': 'sindhi',
-            'si': 'sinhala',
-            'sk': 'slovak',
-            'sl': 'slovenian',
-            'so': 'somali',
-            'es': 'spanish',
-            'su': 'sundanese',
-            'sw': 'swahili',
-            'sv': 'swedish',
-            'tg': 'tajik',
-            'ta': 'tamil',
-            'te': 'telugu',
-            'th': 'thai',
-            'tr': 'turkish',
-            'uk': 'ukrainian',
-            'ur': 'urdu',
-            'ug': 'uyghur',
-            'uz': 'uzbek',
-            'vi': 'vietnamese',
-            'cy': 'welsh',
-            'xh': 'xhosa',
-            'yi': 'yiddish',
-            'yo': 'yoruba',
-            'zu': 'zulu',
+            'af': 'afrikaans',               'sq': 'albanian', 'am': 'amharic', 'ar': 'arabic', 'hy': 'armenian', 'az': 'azerbaijani', 'eu': 'basque',
+            'be': 'belarusian',              'bn': 'bengali', 'bs': 'bosnian', 'bg': 'bulgarian', 'ca': 'catalan', 'ceb': 'cebuano', 'ny': 'chichewa',
+            'zh-cn': 'chinese (simplified)', 'zh-tw': 'chinese (traditional)', 'co': 'corsican', 'hr': 'croatian', 'cs': 'czech', 'da': 'danish',
+            'nl': 'dutch',                   'en': 'english', 'eo': 'esperanto', 'et': 'estonian', 'tl': 'filipino', 'fi': 'finnish', 'fr': 'french', 'fy': 'frisian',
+            'gl': 'galician',                'ka': 'georgian', 'de': 'german', 'el': 'greek', 'gu': 'gujarati', 'ht': 'haitian creole', 'ha': 'hausa', 'haw': 'hawaiian',
+            'iw': 'hebrew',                  'he': 'hebrew', 'hi': 'hindi', 'hmn': 'hmong', 'hu': 'hungarian', 'is': 'icelandic', 'ig': 'igbo', 'id': 'indonesian',
+            'ga': 'irish',                   'it': 'italian', 'ja': 'japanese', 'jw': 'javanese', 'kn': 'kannada', 'kk': 'kazakh', 'km': 'khmer', 'ko': 'korean',
+            'ku': 'kurdish (kurmanji)',      'ky': 'kyrgyz', 'lo': 'lao', 'la': 'latin', 'lv': 'latvian', 'lt': 'lithuanian', 'lb': 'luxembourgish',
+            'mk': 'macedonian',              'mg': 'malagasy', 'ms': 'malay', 'ml': 'malayalam', 'mt': 'maltese', 'mi': 'maori', 'mr': 'marathi', 'mn': 'mongolian',
+            'my': 'myanmar (burmese)',       'ne': 'nepali', 'no': 'norwegian', 'or': 'odia', 'ps': 'pashto', 'fa': 'persian', 'pl': 'polish', 'pt': 'portuguese',
+            'pa': 'punjabi',                 'ro': 'romanian', 'ru': 'russian', 'sm': 'samoan', 'gd': 'scots gaelic', 'sr': 'serbian', 'st': 'sesotho', 'sn': 'shona',
+            'sd': 'sindhi',                  'si': 'sinhala', 'sk': 'slovak', 'sl': 'slovenian', 'so': 'somali',
+            'es': 'spanish',                 'su': 'sundanese',
+            'sw': 'swahili',                 'sv': 'swedish',
+            'tg': 'tajik',                   'ta': 'tamil',
+            'te': 'telugu',                  'th': 'thai',
+            'tr': 'turkish',                 'uk': 'ukrainian', 'ur': 'urdu',
+            'ug': 'uyghur',                  'uz': 'uzbek', 'vi': 'vietnamese',
+            'cy': 'welsh',                   'xh': 'xhosa', 'yi': 'yiddish',
+            'yo': 'yoruba',                  'zu': 'zulu',
 
     <HuggingFaceLM>: Callable [string with [mask]-> string]
 
@@ -182,7 +99,22 @@ class txtAugmentation:
         return txt
 
     def doBackTranslation(self, txt:str):
-        txt = self.translator.translate(txt,src=self.transformation['srcLang'],tmp=self.transformation['tgtLang'], sleeping=self.sleep)
+        sleep = self.sleep
+        try:
+            txt = self.translator.translate(txt,src=self.transformation['srcLang'],tmp=self.transformation['tgtLang'], sleeping=sleep)
+        except:
+            sleep +=6
+            time.sleep(sleep)
+            try:
+               txt = self.translator.translate(txt,src=self.transformation['srcLang'],tmp=self.transformation['tgtLang'], sleeping=sleep)
+            except:
+                sleep *=10
+                time.sleep(sleep)
+                try:
+                    txt = self.translator.translate(txt,src=self.transformation['srcLang'],tmp=self.transformation['tgtLang'], sleeping=sleep)
+                except:
+                    raise RuntimeError('google translate has blocked your request, try again later.')
+
         return txt.result_text
 
 
