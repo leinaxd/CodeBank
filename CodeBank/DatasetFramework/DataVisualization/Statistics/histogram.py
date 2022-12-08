a616d942380c8e63cb536c2f9b983d768193923c
leinaxe <leinaxd@gmail.com>
2022-12-08 13:56:09 -0300

    modified:   CodeBank/DatasetFramework/DataVisualization/Statistics/histogram.py

--- CodeBank/DatasetFramework/DataVisualization/Statistics/histogram.py
+++ CodeBank/DatasetFramework/DataVisualization/Statistics/histogram.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.neighbors import KernelDensity
 
 class histogram:
     """
@@ -26,7 +27,8 @@ class histogram:
         self.range = range
         self.window = window
         self.normalize=normalize
-        self.plot_args = plot_args
+        self.plot_args = {'fill':False}
+        self.plot_args.update(plot_args)
 
     def __call__(self, 
         ax:plt.Axes, 
@@ -34,6 +36,15 @@ class histogram:
         histtype='bar', #do barPlot or contour plot
         barWidth_pcn=0.95,
         ):
+        if histtype.lower() in ['gaussian','tophat','epanechnikov','exponential','linear','cosine']: 
+            return self.doKDE(ax, data, histtype)
+        if histtype.lower() in ['bar']: 
+            return self.doBar(ax, data, histtype)
+        if histtype.lower() in ['step']:
+            return self.doStep(ax, data, histtype)
+
+
+    def doBins(self, data):
         if self.range: minData, maxData = self.range
         else:          minData, maxData = min(data), max(data)
 
@@ -52,27 +63,30 @@ class histogram:
             area = sum( bins.values() ) * binWidth
             for k, v in bins.items():
                 bins[k] = v/area
-        if 'color' not in self.plot_args:
-            color = ax._get_lines.get_next_color()
-            self.plot_args.update(color = color)
+        return bins, binWidth
 
-        return list(bins.values()), list(bins.keys()), self.doPlot(ax, bins, binWidth, histtype, barWidth_pcn) #probs, x_axis, patches
+    def doBar(self, ax:plt.Axes, data:np.ndarray, histtype:str):
+        # if 'color' not in self.plot_args:
+        #     color = ax._get_lines.get_next_color()
+        #     self.plot_args.update(color = color)
+        barWidth_pcn = 0.95
+        bins, binWidth = self.doBins(data)
+        lines = ax.bar(bins.keys(),bins.values(),width=binWidth*barWidth_pcn)
+        return list(bins.values()), list(bins.keys()), lines #probs, x_axis, patches
 
-    def doPlot(self, ax:plt.Axes, bins:dict, binWidth, histtype, barWidth_pcn):
-        if histtype == 'bar':
-            lines = ax.bar(bins.keys(),bins.values(),width=binWidth*barWidth_pcn, **self.plot_args,)
-        elif histtype == 'step':
-            x, y = [None], [0]
-            for pos, prob in bins.items():
-                x.append(pos-binWidth/2)
-                x.append(pos+binWidth/2)
-                y.append(prob)
-                y.append(prob)
-            x[0] = x[1] #start & end with zero prob
-            x.append(x[-1])
-            y.append(0)
-            lines = ax.plot(x, y, **self.plot_args)
-        
+    def doStep(self, ax:plt.Axes, data:np.ndarray, histtype:str):
+        bins, binWidth = self.doBins(data)
+        x, y = [None], [0]
+        for pos, prob in bins.items():
+            x.append(pos-binWidth/2)
+            x.append(pos+binWidth/2)
+            y.append(prob)
+            y.append(prob)
+        x[0] = x[1] #start & end with zero prob
+        x.append(x[-1])
+        y.append(0)
+        if self.plot_args['fill']: lines = ax.fill(x, y, **self.plot_args)
+        else:                      lines = ax.plot(x, y)
         # nTicks = 5
         # tick_range = max(bins.values())
         # msd = np.round_(np.log10(tick_range), 0)
@@ -80,9 +94,19 @@ class histogram:
         
         # ticks = np.arange(0, tick_range+tick_step, tick_step)
         # ax.set_yticks(ticks)
-        return lines
+        return list(bins.values()), list(bins.keys()), lines #probs, x_axis, patches
     
-
+    def doKDE(self, ax:plt.Axes, data:np.ndarray, histtype:str):
+        bandwidth = 1/self.nBins
+        bandwidth = 1
+        
+        x_axis = np.linspace(min(data),max(data),10*self.nBins)[:,np.newaxis]
+        if len(data.shape) == 1: data = data[:,np.newaxis]
+        kde     = KernelDensity(kernel=histtype,bandwidth=bandwidth).fit(data)
+        logProb = kde.score_samples(x_axis)
+        probs   = np.exp(logProb)
+        lines = ax.fill_between(x_axis[:,0],probs,alpha=0.7)
+        return probs, x_axis, lines
 
 
 if __name__ == '__main__':
@@ -91,18 +115,26 @@ if __name__ == '__main__':
         import numpy as np
         plt.style.use('dark_background')
         size = 10000
-        uniform = np.random.random(size)*100
-        gaussian = np.random.randn(size)
+        data_u = np.random.random(size)
+        data_g = np.random.randn(size)
 
-        data_g = gaussian
-        data_u = uniform
 
         print(f"test {test}")
         hist = histogram(10)
-        fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2)
+        fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6), (ax7, ax8)) = plt.subplots(4,2, figsize=(6.4,6.8))
         hist(ax1, data_g, histtype='bar')
         hist(ax2, data_u, histtype='bar')
         hist(ax3, data_g, histtype='step')
         hist(ax4, data_u, histtype='step')
+        hist(ax5, data_g, histtype='gaussian')
+        hist(ax6, data_u, histtype='gaussian')
+        hist(ax7, data_g, histtype='epanechnikov')
+        hist(ax8, data_u, histtype='epanechnikov')
 
+        plt.show()
+
+    if test == 2:
+        size = 100
+        data_g = np.random.randn(size,1)
+        plt.style.use('dark_background')
         plt.show()
